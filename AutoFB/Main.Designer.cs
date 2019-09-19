bb308c7bd559342cdb3b2abe5998d8773744df1a
Kusyun <kusyunexcl@gmail.com>
2019-09-19 22:11:27 +0800

    建立專案 1、新增FBClient類別 2、新增基礎操作類別 3、新增登入

--- AutoFB/Main.Designer.cs
+++ AutoFB/Main.Designer.cs
@@ -35,7 +35,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(289, 52);
+            this.button1.Location = new System.Drawing.Point(156, 52);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(101, 51);
             this.button1.TabIndex = 0;
